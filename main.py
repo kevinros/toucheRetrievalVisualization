e1a67a845ad7214e78ba3023d98ebcbca42b5599
Kevin Ros <kjros2@timan107.cs.illinois.edu>
2021-05-05 11:48:46 -0500

    flag descriptions

--- main.py
+++ main.py
@@ -36,9 +36,13 @@ path_to_topics = 'metadata/topics-task-1-only-titles.xml'
 # relevance scores path
 path_to_qrels = 'metadata/touche2020-task1-relevance-args-me-corpus-version-2020-04-01-corrected.qrels'
 
+# flag to create the directories, create the Pyserini index, and embedd the corpus
 initialize = False
+# flag to load the indexes
 setup = True
-evaluate = False
+# flag to run the searches
+evaluate = True
+# flag to view some results (setup must be true)
 view = True
 
 if initialize:
@@ -57,7 +61,7 @@ if setup:
     # load the semantic knn index + passage lookup files
     hnswlib_index = hnswlib.Index(space = 'cosine', dim=768)
     hnswlib_index.load_index(path_to_semantic_output + 'passage.index')
-    hnswlib_index.set_ef(250)
+    hnswlib_index.set_ef(1100)
     
     idx_to_passageid = pickle.load(open(path_to_semantic_output + 'idx_to_passageid.p', 'rb'))
     idx_to_passage = pickle.load(open(path_to_semantic_output + 'idx_to_passage.p', 'rb'))
@@ -84,29 +88,29 @@ if evaluate:
     # Run BM25
     bm25_run = searcher.bm25Search(pyserini_searcher, topics)
     processor.writeRelevanceFile(bm25_run, path_to_run_output + 'run.bm25', 'bm25')
-    #os.system('../trec_eval/./trec_eval -m ndcg_cut.20 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.bm25')
+    #os.system('../trec_eval/./trec_eval -m ndcg_cut.5 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.bm25')
 
     # Run semantic search
     semantic_run = searcher.semanticSearch(semantic_model, topics, hnswlib_index, idx_to_passageid)
     processor.writeRelevanceFile(semantic_run, path_to_run_output + 'run.semantic', 'semantic')
-    #os.system('../trec_eval/./trec_eval -m ndcg_cut.20 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.semantic')
+    #os.system('../trec_eval/./trec_eval -m ndcg_cut.5 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.semantic')
 
 
     # Interpolate BM25 and semantic with alpha=0.7
     interpolated_bm25_semantic = reranker.interpolate(path_to_run_output + 'run.bm25', path_to_run_output + 'run.semantic', 0.7)
     processor.writeRelevanceFile(interpolated_bm25_semantic, path_to_run_output + 'run.bm25.semantic', 'bm25-0.7semantic')
-    #os.system('../trec_eval/./trec_eval -m ndcg_cut.20 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.bm25.semantic')
+    #os.system('../trec_eval/./trec_eval -m ndcg_cut.5 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.bm25.semantic')
    
 
     # NN manifold rerank no cutoff
     manifold_run = reranker.nn_pf_manifold(path_to_run_output + 'run.bm25.semantic', semantic_model, topics, hnswlib_index, idx_to_passageid, docid_to_doc)
     processor.writeRelevanceFile(manifold_run, path_to_run_output + 'run.bm25.semantic.manifold', 'manifold')
-    #os.system('../trec_eval/./trec_eval -m ndcg_cut.20 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.bm25.semantic.manifold')
+    #os.system('../trec_eval/./trec_eval -m ndcg_cut.5 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.bm25.semantic.manifold')
 
     # NN manifold rerank with 10 cutoff
     manifold_run_c10 = reranker.nn_pf_manifold(path_to_run_output + 'run.bm25.semantic', semantic_model, topics, hnswlib_index, idx_to_passageid, docid_to_doc, rerank_cutoff=10)
     processor.writeRelevanceFile(manifold_run_c10, path_to_run_output + 'run.bm25.semantic.manifold_c10', 'manifold-c10')
-    #os.system('../trec_eval/./trec_eval -m ndcg_cut.20 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.bm25.semantic.manifold_c10')
+    #os.system('../trec_eval/./trec_eval -m ndcg_cut.5 ' + path_to_qrels + ' ' +  path_to_run_output + 'run.bm25.semantic.manifold_c10')
 
     """
     Additional methods that are not currently used
@@ -139,7 +143,7 @@ if view:
     manifold_c10_run_path = path_to_run_output + 'run.bm25.semantic.manifold_c10'
 
     topic = '91'
-    for path_to_run in [bm25_run_path]:
+    for path_to_run in [manifold_run_path]:
          run = reranker.loadRun(path_to_run)[topic]
          print(topics[topic]['title'])
          for docid,_ in run[:5]:
